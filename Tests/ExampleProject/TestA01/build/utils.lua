582cb32c71b9b46c1e686bf7cf11816d80a43e9e
Caleb B <brodock1234@gmail.com>
2023-08-14 19:23:07 -0700

    Clean

--- Tests/ExampleProject/TestA01/build/utils.lua
+++
@@ -1,3 +0,0 @@
-local function helloWorld()
-    print( "Hello World!")
-end
--- build_dispatcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set "CLuaProjectFile=%~1"
-node --experimental-modules D:/Github/CLua/exposedLayer.mjs "%CLuaProjectFile%"
-pause
-endlocal
